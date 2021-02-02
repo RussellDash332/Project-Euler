e19b3161c5a6e45bca95854a8c9c645a91d75009
RussellDash332 <russellsaerang@gmail.com>
2021-02-02 19:25:05 +0800

    Improved Problem 3

--- pe-003.py
+++ pe-003.py
@@ -1,14 +1,12 @@
 from check_primes import is_prime
 
+# highest prime factor
 def hpf(n):
-    factor = []
-    result = 1
+    result = 1 # some small number
     for i in range(n):
-        if n % (i+1) == 0:
-            factor.append(i+1)
-    for i in range(len(factor)):
-        if is_prime(factor[i]):
-            result = factor[i]
+        if n % (i+1) == 0 and is_prime(i+1):
+            result = i+1
+            print(result) # track
     return result
 
 print(hpf(600851475143))
