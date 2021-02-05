2ed56a7b7ac0426b95c5dab9dd86e8121507a7ca
RussellDash332 <russellsaerang@gmail.com>
2021-02-05 13:14:33 +0800

    Updated number_theory

--- number_theory.py
+++ number_theory.py
@@ -5,3 +5,12 @@ def gcd(x, y):
 
 def lcm(x, y):
     return x*y//gcd(x,y)
+
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        result = 1
+        for i in range(2,n+1):
+            result *= i
+    return result
