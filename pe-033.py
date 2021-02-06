9d0e96f4f4c0ab5eb489c003737e5594c659785a
RussellDash332 <russellsaerang@gmail.com>
2021-02-06 16:04:44 +0800

    Added Problem 33

---
+++ pe-033.py
@@ -0,0 +1,7 @@
+# The answer is 100.
+# Another non-code question.
+# You've seen 16/64 being memed to be 1/4 because it is true.
+# Maybe you don't know about 26/65 but it is 2/5.
+# Why do you forget the given, 49/98? It's 1/2.
+# Finally, 19/95 is 1/5.
+# So, 1/4 * 2/5 * 1/2 * 1/5 is 1/100. We are done.
